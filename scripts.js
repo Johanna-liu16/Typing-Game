f7fe46d56f58a85d3878e1ad5ef8449847cc76c2
Johanna Liu <johanna.liu99@gmail.com>
2025-01-28 23:21:12 -0500

    key recognisation and colour change

--- scripts.js
+++ scripts.js
@@ -2,42 +2,86 @@
 // Created by: Johanna Liu
 // Date: Jan 2024
 
-// create words from api
+function addClass(element, name) {
+    if (!element.classList.contains(name)) {
+        element.classList.add(name);
+    }
+}
+
+function removeClass(element, name) {
+    if (element.classList.contains(name)) {
+        element.classList.remove(name);
+    }
+}
+
+// Fetch a random word from the API
 function generateWords() {
     return fetch('https://random-word-api.herokuapp.com/word')
         .then(response => response.json())
         .then(data => data[0]);
 }
 
-// get the created word
-async function getWords() {
-
-    var word = await generateWords();
-    return word;
-}
-
-//make array of words
+// Create an array of words
 async function renderWords() {
-    var quote = [];
+    const wordArray = [];
+    const wordCount = 25;
 
-   for (var i = 0; i < 25; i++) {
-        aWord = await getWords();
-        quote.push(aWord);
-    }
-    quote = quote.concat(quote)
-    quote = quote.join("\n")
-    document.getElementById('words').innerHTML = quote
-
-    // separate each character
-    for (var j = 0; j <quote.length; j++) {
-        document.getElementById('words').innerHTML += (`<span>${quote[j]}</span>`)
+    // Generate words and store them in an array
+    for (let i = 0; i < wordCount; i++) {
+        const word = await generateWords();
+        wordArray.push(word);
     }
 
-    //detect keyborad actions 
-    document.getElementById('game').addEventListener('keyup', event => {
-        console.log(event)
-    })
+    // Render the words as spans
+    const wordsContainer = document.getElementById('words');
+    wordsContainer.innerHTML = ''; // Clear the container
+    wordArray.join(' ').split('').forEach((char, index) => {
+        const span = document.createElement('span');
+        span.className = 'letter';
+        span.textContent = char;
+        if (index === 0) addClass(span, 'current');
+        wordsContainer.appendChild(span);
+    });
+
+    // Start tracking typing input
+    trackTyping();
 }
 
+// Typing logic
+function trackTyping() {
+    const wordsContainer = document.getElementById('words');
+    const letters = wordsContainer.querySelectorAll('.letter');
+    let currentIndex = 0;
+
+    document.addEventListener('keydown', (event) => {
+        const key = event.key;
+        const currentLetter = letters[currentIndex];
+        const expected = currentLetter.innerHTML;
+        const isLetter = key.length === 1 && key !== ' ';
+        const isSpace = key === ' '
+
+        console.log({key, expected})
+
+        if (isLetter) {
+            if (currentLetter) {
+                addClass(currentLetter, key === expected ? 'correct' : 'wrong')
+                currentIndex ++
+            }
+        }
+
+        if (isSpace) {
+            if (expected !== ' ') {
+                addClass(currentLetter, 'wrong')
+                currentIndex++
+            } else {
+                addClass(currentLetter, 'correct')
+                currentIndex++
+            }
+
+        }
+    });
+}
+
+// Initialize the game
 renderWords();
 
